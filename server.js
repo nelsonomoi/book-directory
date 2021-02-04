8721fb4b47f66faffa8b3ca6e6af8175316a39a6
nelsonomoi <omoinelson3@gmail.com>
2021-02-04 22:30:38 +0300

    fetched all books

--- server.js
+++ server.js
@@ -15,21 +15,19 @@ var app = express()
 var conn = mysql.createConnection(config)
 
 app.use(bodyParser.urlencoded({extended: false}))
+
 app.use(express.static(__dirname+'/public/'))
 
 app.get('/',function(req,res){
-    var all_books = null
-    conn.connect(function(err){
-        if(err) throw err
-        var sql = "SELECT * FROM BOOKS ORDER BY UPDATED_AT ";
-        conn.query(sql,function(err,results){
-            if(err) throw err;
-            
-            all_books = results
-        })
-    })
-
     res.sendFile('index.html',{ root: path.join(__dirname,'/src')})
+})
+
+app.get('/all-books',function(req,res) {
+    var sql = "SELECT * FROM BOOKS ORDER BY UPDATED_AT ";
+    conn.query(sql,function(err,results){
+        if(err) throw err;
+        res.send(JSON.stringify(results))
+    })
 })
 
 
